bd5ecf8438945066b8eb132db9f4b6f2d252ffae
TR Murphy <biffsocko@gmail.com>
2023-05-03 21:00:10 -0400

    Update jokes_client.py

--- jokes_client.py
+++ jokes_client.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
+#############################################
+# TR Murphy
+# Jokes_client.py
+#############################################
 
 import socket
 
-#HOST = "10.32.67.14"   # Replace with the hostname or IP address of the server
-HOST = "research-jump-001.fcstone.com"   # Replace with the hostname or IP address of the server
-#PORT = 65433  # Replace with the port number used by the server
-PORT = 10000  # Replace with the port number used by the server
+#HOST = "10.32.67.14"   
+HOST = "someserver.goes_here.com"   # Replace with the hostname or IP address of the server
+#PORT = 65433  
 
 # Create a socket object and connect to the server
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
+###########################################################
+# don't need this - but kept it commented for notes later
+###########################################################
 # Send a message to the server
 #message = 'Hello, server!'
 #sock.send(message.encode())
